3a958224d07659acc2bd14dae9a8a6caafbf8a0a
sai.putravu <sai.putravu@student.manchester.ac.uk>
2025-03-14 13:50:59 +0000

    Added basic HTTP Server that accepts POST on /submit.
    TODO:
    - Fix the API tokens -> read from file.
    - Fix the response from a connection.
    - Check the input is passed in (equities list).

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"mtschal/internal"
+)
+
+const (
+	defaultPort    = 8080
+	defaultAddress = "localhost"
+)
+
+func main() {
+	// Define command-line flags for address and port.
+	addr := flag.String("addr", defaultAddress, "Server address to bind to")
+	port := flag.Int("port", defaultPort, "Server port to listen on")
+	flag.Parse()
+
+	http.HandleFunc("/submit", internal.PostHandler)
+	log.Printf("Starting server on port %d", *port)
+
+	net := fmt.Sprintf("%s:%d", *addr, *port)
+	if err := http.ListenAndServe(net, nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
---
+++ internal/handlers.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+// postHandler handles POST requests to the /postpath endpoint.
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	// Only allow POST requests.
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check for the API code in the header "X-API-Code".
+	// FIXME: check this with many apis else error
+	apiKey := r.Header.Get("X-API-Code")
+	if apiKey != "test" {
+		http.Error(w, "Unauthorized - invalid API key", http.StatusUnauthorized)
+		return
+	}
+
+	// Read the request body.
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// Prepare a response.
+	// FIXME: Calculate response
+	resp := Response{
+		Message: "Request accepted, payload: " + string(body),
+	}
+
+	// Respond with JSON.
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
