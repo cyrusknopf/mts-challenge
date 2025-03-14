2a81af7a8304bc1980d09628bd7db92f5804c5a2
Cyrus Knopf <cyrus.knopf@gmail.com>
2025-03-14 13:02:16 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      pkgsFor = forSystem (system :
+        import nixpkgs { inherit system; }
+      );
+    in
+    {
+      devShells = forSystem
+        (system:
+          let
+            pkgs = pkgsFor."${system}";
+          in
+            {
+              default = pkgs.mkShell {
+                buildInputs = with pkgs;
+                  [
+                    zsh
+
+                    go 
+                    gopls
+
+                    protobuf
+
+                    tree
+                    cbonsai
+                  ];
+                shellHook = ''
+                    export PATH=${pkgs.zsh}/bin:$PATH
+                    export SHELL=${pkgs.zsh}/bin/zsh
+                    export NIX_FLAKE_PROMPT="(fenrir)"  # Change this per flake!
+                    exec ${pkgs.zsh}/bin/zsh --login
+                '';
+              };
+            }
+        );
+    };
+}
